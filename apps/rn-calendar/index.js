85a3c461485dcf6cd4011d7642b490210c7ed476
WJH <3506456886@qq.com>
2026-01-01 19:52:18 +0800

    fix: intl依赖缺失

--- apps/rn-calendar/index.js
+++ apps/rn-calendar/index.js
@@ -1,6 +1,9 @@
 /**
  * @format
  */
+// Polyfill for Intl.PluralRules (required by i18next)
+import 'intl-pluralrules';
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
